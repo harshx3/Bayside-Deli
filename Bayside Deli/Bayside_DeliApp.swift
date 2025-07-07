f665c44d79fd1fc08aff1584c0cdc67ed95d2597
harshx3 <harshmakwana686@gmail.com>
2025-07-07 17:31:06 -0400

    Project Added to Git

---
+++ Bayside Deli/Bayside_DeliApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bayside_DeliApp.swift
+//  Bayside Deli
+//
+//  Created by Harsh Makwana on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Bayside_DeliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bayside DeliTests/Bayside_DeliTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bayside_DeliTests.swift
+//  Bayside DeliTests
+//
+//  Created by Harsh Makwana on 7/7/25.
+//
+
+import Testing
+@testable import Bayside_Deli
+
+struct Bayside_DeliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
